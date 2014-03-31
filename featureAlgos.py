19531d571d5c76f70745e280bc47c161d4e23a8b
Julian Ramos -Desktop CMU <ingenia@andrew.cmu.edu>
2014-03-31 19:25:18 -0400

    Finally the mutual information is calculated. Tested already for a one dimensional data set. Still needs to be tested for more than one dimension. This current version will not scale well however is so simple that may also work on a big data set. It is strange that this version didn't work at all for less than 2 dimensions. Anyways I change the digitize which was giving problems and currently is working fine but still need to make changes to multidimensional code.

--- featureAlgos.py
+++ featureAlgos.py
@@ -17,6 +17,81 @@ def entropyVector(x):
 #     plt.plot(x)
 #     plt.show()
     return entro
+def mutualInformation(jointProb):
+    '''
+    Calculates the mutual information from the output of
+    the jointProbs function
+    '''
+    
+    
+
+def jointProbs(x,y):
+    '''
+    Calculates the probabilities of joint probabilities for the different values of x and y
+    The values most be discrete,positive, starting from 0 or 1
+    '''
+    probs={}
+    
+    valsX,temp=np.unique(x,return_inverse=True)
+    valsX=[int(i) for i in valsX]
+    temp=np.bincount(temp) 
+    pX=temp/float(sum(temp))
+    
+    valsY,temp=np.unique(y,return_inverse=True)
+    valsY=[int(i) for i in valsY]
+    temp=np.bincount(temp) 
+    pY=temp/float(sum(temp))
+    
+    maxX=max(valsX)
+    maxY=max(valsY)
+    
+    minX=min(valsX)
+    minY=min(valsY)
+    
+    C={}
+    
+    #Another option would be to use sparse matrices however that will 
+    #only work for 2 dimensional matrices 
+
+    #This is a very efficient version of the algorithm
+    for i in range(len(x)):
+        key=str(x[i])+','+str(int(y[i]))
+        if key in C.keys():
+            C[key]+=1
+        else:
+            C[key]=1
+        
+    
+    den=0
+    for xi in valsX:
+        for yi in valsY:
+            key=str(xi)+','+str(yi)
+            if key in C.keys():
+                probs[key]=C[key]
+                den+=C[key]
+            else:
+                probs[key]=0
+    
+    for (key,val) in probs.iteritems():
+        probs[key]=probs[key]/float(den)
+       
+    
+    totalSum=0
+    for key,val in probs.iteritems():
+        xVal,yVal=[int(i) for i in key.split(',')]
+        
+        indX=valsX.index(xVal)
+        indY=valsY.index(yVal)
+        if probs[key]==0:
+            totalSum+=0
+        else:
+            totalSum+=probs[key]*np.log(probs[key]/(pX[indX]*pY[indY]))
+        
+        
+     
+    
+#     return {'counts':C,'probabilities':probs}
+    return totalSum
     
 
 
@@ -26,9 +101,13 @@ def informationGain(x,y):
     simply binerizes the data and then
     calculates information gain following the
     elements of information theory book equation in page
-    x : features to be analyzed, where rows are data points and columns correspond to features
-    y : class labels
+    x : features to be analyzed, where rows are data points and columns correspond to features 
+    y : class labels 
+    here Information gain is:
+    I(y;x)=H(y)-H(y|x)   
+    I(y;x)=\sum_{x,y} p(x,y)\frac {log(p(x,y))}{(p(x).p(y))} 
     '''
+    
     x=np.array(x)
     y=np.array(y)
     
@@ -38,39 +117,39 @@ def informationGain(x,y):
     
     #Discretized data
     xD=[]
-    
-    for i in range(len(xMin)):
-        #-1e-5 and+1e-5 added so that the extremes are included in the 
-        #bins, this however generates two empty bins that's why the last
-        #[1:-2]
-        bins.append(np.linspace(xMin[i]-1e-5,xMax[i]+1e-5, num=10))
-        xD.append(np.digitize(x[:,i],bins[i]))
-#         entros.append(entropy(xD[i]))
-    
-    # Now I need to calculate the join entropy for 
-    # the class and the feature for that should be counted 
-    # the times the different values of the class and
-    # the feature as discretized occur. I could use here the
-    # find multiple function or I could join the two 
-    # vectors into one array and then just check them both??
-    
-    
-#     plt.plot(x[:,0])
-#     plt.plot(xD[0])
-#     plt.show()
-        
-    print(entropyVector(xD[0]))
-#     plt.hist(xD,10)
-#     plt.hold(True)
-    plt.hist(x*10,10)
-    plt.show()
+    try:
+        for i in range(len(xMin)):
+            #-1e-5 and+1e-5 added so that the extremes are included in the 
+            #bins, this however generates two empty bins that's why the last
+            #[1:-2]
+            tempXD,tempBins=np.histogram(x[:,i],10)
+            xD.append(np.digitize(x,tempBins))
+            bins.append(tempBins)
+            C=jointProbs(xD[-1],y)
+            print(C)
+    except:
+#         bins.append(np.linspace(xMin-1e-5,xMax+1e-5, num=10))
+        binsNum=len(np.unique(x))
+        if binsNum<10:
+            temp,tempBins=np.histogram(x,binsNum)
+            xD.append(np.digitize(x,tempBins))
+        else:
+            temp,tempBins=np.histogram(x,10)
+            xD.append(np.digitize(x,tempBins))
+        C=jointProbs(xD[-1],y)
+    return C
+
     
 
 
 if __name__=='__main__':
-    x=np.random.rand(900,3)
-    y=np.round(np.random.rand(900,1))
-    informationGain(x,y)
+#     x=np.random.rand(5,2)
+#     y=np.round(np.random.rand(5,1))
+    x=[1,1,0,1]
+    y=[0,0,1,1]
+    print(x)
+    print(y)
+    print(informationGain(x,y)) 
     
     
     
